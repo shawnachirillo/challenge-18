9a14e0d20434eb2f1866b1e1f01a5d1a0197dace
Shawna Chirillo <schirillo711@gmail.com>
2025-04-14 11:39:48 -0500

    fix: switch to tsx and update config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
     open: true,
     proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
+      '/graphql': {
+        target: 'http://localhost:3002',
         secure: false,
         changeOrigin: true
       }
