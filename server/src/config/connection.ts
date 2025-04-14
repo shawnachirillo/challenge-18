ba0362b48bfba994ffa0fc2ef7e87332d2ef4797
Shawna Chirillo <schirillo711@gmail.com>
2025-04-14 12:14:45 -0500

    changes

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -9,7 +9,7 @@ const MONGODB_URI = process.env.MONGODB_URI
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(MONGODB_URI);
+    await mongoose.connect(MONGODB_URI || '');
     console.log('✅ MongoDB connected');
   } catch (error) {
     console.error('❌ MongoDB connection error:', error);
