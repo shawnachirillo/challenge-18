e895ab88190d24930c43e787329c1086f8c3c8e6
Shawna Chirillo <schirillo711@gmail.com>
2025-04-14 12:33:41 -0500

    changes

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -5,11 +5,11 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const MONGODB_URI = process.env.MONGODB_URI 
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks';
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(MONGODB_URI || '');
+    await mongoose.connect(MONGODB_URI);
     console.log('✅ MongoDB connected');
   } catch (error) {
     console.error('❌ MongoDB connection error:', error);
